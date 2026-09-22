21f8348e8eb038a7d897d8f35142cbda7d4560c7
drew25927 <drew25927@gmail.com>
2026-09-22 14:27:12 +0900

    Force dynamic rendering and disable fetch caching on admin API routes

--- app/api/admin/login/route.js
+++ app/api/admin/login/route.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { NextResponse } from 'next/server';
 import { supabaseAdmin } from '../../../../lib/supabaseAdmin';
 import { verifyPassword } from '../../../../lib/adminAuth';
